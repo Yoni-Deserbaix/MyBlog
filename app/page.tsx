778c9c72da808fa6b88565799466d2aba8b834dd
unknown <yonideserbaix@gmail.com>
2024-02-17 10:05:19 +0100

    fetching data with TypeScript

--- app/page.tsx
+++ app/page.tsx
@@ -2,12 +2,28 @@ import Image from "next/image";
 import Link from "next/link";
 import Button from "./ui/Button";
 
-export default function Home() {
+const getData = async () => {
+  const res = await fetch("http://localhost:4000/articles");
+  const data = await res.json();
+  return data;
+};
+
+type Article = {
+  id: number;
+  title: string;
+  author: string;
+  date: string;
+  user_id: number;
+};
+
+export default async function Home() {
+  const articles = await getData();
+  console.log(articles);
 
   return (
     <main className="">
       <h1>Welcome ! </h1>
-      
+
       {/* route conventionnelle */}
       <Link href="/login">Login</Link>
       <br />
@@ -18,6 +34,15 @@ export default function Home() {
       <br />
       {/* Client side component */}
       <Button />
+
+      <div className="container">
+        {articles.map((article: Article) => (
+          <div key={article.id} className="card">
+            <h3>{article.id}</h3>
+            <p>{article.author}</p>
+          </div>
+        ))}
+      </div>
     </main>
   );
 }
