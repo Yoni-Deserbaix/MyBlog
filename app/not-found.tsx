12f13c0a2ca99609e7168003c945380a25367e9b
unknown <yonideserbaix@gmail.com>
2024-02-24 22:07:04 +0100

    update not found page

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -3,5 +3,9 @@ import React from "react";
 export interface IAppProps {}
 
 export default function NotFound(props: IAppProps) {
-  return <div>Not found</div>;
+  return (
+    <div>
+      <h1 className="titre">Not found</h1>
+    </div>
+  );
 }
