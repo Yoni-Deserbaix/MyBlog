6e7025ad98ee2ecbbf5b2b59901dcbd02d06da94
unknown <yonideserbaix@gmail.com>
2024-02-26 18:00:55 +0100

    add font semibold style

--- app/ui/Navbar.tsx
+++ app/ui/Navbar.tsx
@@ -70,12 +70,12 @@ export default function NavBar() {
               }`}
             >
               <ul className="h-screen md:h-auto items-center justify-center md:flex ">
-                <li className="pb-6 text-xl text-white py-2 md:px-6 text-center border-b-2 md:border-b-0  hover:bg-purple-900  border-purple-900  md:hover:text-purple-600 md:hover:bg-transparent">
+                <li className="pb-6 text-xl text-white py-2 md:px-6 text-center border-b-2 md:border-b-0  hover:bg-purple-900  border-purple-900  md:hover:text-purple-600 md:hover:bg-transparent font-semibold">
                   <Link href="/" onClick={() => setNavbar(!navbar)}>
                     Blog
                   </Link>
                 </li>
-                <li className="pb-6 text-xl text-white py-2 px-6 text-center  border-b-2 md:border-b-0  hover:bg-purple-600  border-purple-900  md:hover:text-purple-600 md:hover:bg-transparent">
+                <li className="pb-6 text-xl text-white py-2 px-6 text-center  border-b-2 md:border-b-0  hover:bg-purple-600  border-purple-900  md:hover:text-purple-600 md:hover:bg-transparent font-semibold">
                   <Link
                     href="https://yoni-deserbaix.github.io/Portfolio/"
                     target="_blank"
