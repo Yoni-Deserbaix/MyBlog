a99030ce20c525101998bc99e7b2176fb9070eea
unknown <yonideserbaix@gmail.com>
2024-03-23 18:47:05 +0100

    update footer

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -4,7 +4,7 @@ export default function App() {
   return (
     <div>
       <footer className="mt-28  m-4 text-center text-xs text-muted-foreground">
-        &copy; {new Date().getFullYear()} Yoni Deserbaix
+        &copy; {new Date().getFullYear()} Yoni Deserbaix - All Rights Reserved.
       </footer>
     </div>
   );
