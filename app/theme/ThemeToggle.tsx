8443f8dd13e495af06985370c147bf9a8a63cd16
unknown <yonideserbaix@gmail.com>
2024-03-23 21:47:46 +0100

    add aria-label

--- app/theme/ThemeToggle.tsx
+++ app/theme/ThemeToggle.tsx
@@ -10,6 +10,7 @@ export const ThemeToggle = () => {
     <div>
       <Button
         variant="ghost"
+        aria-label="toggle theme"
         onClick={() => {
           setTheme(theme === "light" ? "dark" : "light");
         }}
