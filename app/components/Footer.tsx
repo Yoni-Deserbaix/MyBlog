40afa4943cfc3cc0049f92676317341de078be50
unknown <yonideserbaix@gmail.com>
2024-05-02 13:36:20 +0200

    style(footer): correct position

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -1,28 +1,44 @@
-import { Github, Linkedin } from "lucide-react";
+import { Github, Linkedin, Mail } from "lucide-react";
 import Link from "next/link";
 
-export default function App() {
+export default function Footer() {
   return (
-    <footer className="m-10 text-center text-lg">
-      <div className="font-bold underline underline-offset-8 decoration-green-500 rotate-2 hover:text-green-400 transition-all">
-        Yoni Deserbaix 🦇
+    <div>
+      <div className="max-w-6xl m-auto">
+        <div className="border-t mt-10"></div>
+        <div className="flex">
+          <div className="mx-auto p-10">
+            <div className="text-xl font-bold underline underline-offset-8 decoration-green-500 -rotate-2">
+              Yoni Deserbaix 🦇
+            </div>
+            <div className="p-5 gap-5 flex items-center justify-center">
+              <Link
+                href="https://www.linkedin.com/in/yoni-deserbaix/"
+                target="_blank"
+                aria-label="Navigate to the LinkedIn account"
+                className="hover:scale-125 transition-all"
+              >
+                <Linkedin size={22} />
+              </Link>
+              <Link
+                href="https://github.com/Yoni-Deserbaix"
+                target="_blank"
+                aria-label="Navigate to the Github account"
+                className="hover:scale-125 transition-all"
+              >
+                <Github size={22} />
+              </Link>
+              <Link
+                href="mailto:yonideserbaix@gmail.com"
+                className="hover:scale-125 transition-all z-10"
+                aria-label="Email me at yonideserbaix@gmail.com"
+              >
+                <Mail size={22} />
+              </Link>
+            </div>
+          </div>
+        </div>
       </div>
-      <div className="mt-3 gap-4 flex items-center justify-center  ">
-        <Link
-          href="https://www.linkedin.com/in/yoni-deserbaix/"
-          target="_blank"
-          aria-label="Navigate to the LinkedIn account"
-        >
-          <Linkedin size={20} />
-        </Link>
-        <Link
-          href="https://github.com/Yoni-Deserbaix"
-          target="_blank"
-          aria-label="Navigate to the Github account"
-        >
-          <Github size={20} />
-        </Link>
-      </div>
-    </footer>
+    </div>
   );
 }
